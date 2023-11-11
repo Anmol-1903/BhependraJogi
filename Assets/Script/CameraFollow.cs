c70ffd2872113b8a75356bda707d5623eb2dde51
Anmol-1903 <anmolmittal1903@gmail.com>
2023-11-11 23:11:45 +0530

    Bhupendra Jogi
    Bhupendra Jogi

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    [SerializeField] bool _takeOffsetFromInitialPosition;
+    [SerializeField] Vector3 _offset;
+    [Range(0f,0.2f)]
+    [SerializeField] float _laziness;
+    private void Awake()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void Start()
+    {
+        if(_takeOffsetFromInitialPosition)
+        {
+            _offset = transform.position - _target.position;
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector3 currentVelocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, _target.position + _offset, ref currentVelocity, _laziness);
+    }
+}
